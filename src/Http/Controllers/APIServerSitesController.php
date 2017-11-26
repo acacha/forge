322296c853153e6dbe95b29e2c04f836be726a72
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-11-26 14:49:57 +0100

    Sync

--- src/Http/Controllers/APIServerSitesController.php
+++ src/Http/Controllers/APIServerSitesController.php
@@ -4,6 +4,7 @@ namespace Acacha\Forge\Http\Controllers;
 
 use Acacha\Forge\Http\Requests\ServerSitesStore;
 use Acacha\Forge\Models\Server;
+use Log;
 use Themsaid\Forge\Exceptions\TimeoutException;
 use Themsaid\Forge\Forge;
 
@@ -40,13 +41,18 @@ class APIServerSitesController extends Controller
      */
     public function store(ServerSitesStore $request, $forgeserver)
     {
+        Log::info('store method on APIServerSitesController');
+
         $response = [];
         try {
             $response = $this->forge->createSite($forgeserver, $request->only(['domain','project_type','directory']),true);
         } catch (TimeoutException $exception) {
+            Log::info('Timeout exception!!');
             abort(500,'Timeout exception connecting to Laravel Forge');
         }
 
+        Log::info('Before end');
+        Log::error($response);
         return (array) $response;
     }
 
